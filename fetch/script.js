c80294a695cbc6b1ced3f77413dfc91dc4086d50
Ralph dos Reis Torres <ralphdosreistorres@gmail.com>
2026-03-02 12:33:14 -0300

    add async-await

--- fetch/script.js
+++ fetch/script.js
@@ -8,3 +8,37 @@ const requisicao = fetch('https://ranekapi.origamid.dev/wp-json/api/produto')
   });
 
 console.log(requisicao);
+
+// Alternativa
+fetch('https://ranekapi.origamid.dev/wp-json/api/produto')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  });
+
+// Post
+const data = {
+  id: 'andrerafa',
+  nome: 'Andre',
+  email: 'andrerafa@origamid.com',
+  senha: '123456',
+  cep: '123456',
+  rua: 'Ali Perto',
+  numero: '230',
+  bairro: 'Botafogo',
+  cidade: 'Rio de Janeiro',
+  estado: 'Rio de Janeiro',
+};
+
+fetch('https://ranekapi.origamid.dev/wp-json/api/usuario', {
+  method: 'POST',
+  mode: 'cors',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  redirect: 'follow',
+  referrer: 'no-referrer',
+  body: JSON.stringify(data),
+});
